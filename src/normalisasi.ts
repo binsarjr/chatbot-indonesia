8b2d52cbe209af2f65c1f99dc706c431e8803193
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2020-10-06 19:43:46 +0700

    Normalisasi data sentimen

--- src/normalisasi.ts
+++ src/normalisasi.ts
@@ -1,37 +1,29 @@
 import fs from "fs";
-import { wrap } from "module";
 import { Tokenizer,Cleaner } from "./nlp";
 
 const stopword = require('stopword')
 
-let data: any = fs.readFileSync('./normalisasi.txt').toString()
-let sentences = Tokenizer.sentence_tokenizer(data)
 
-let results: any[] = []
-fs.writeFileSync('./normal.txt','')
-sentences.forEach((sentence: string) => {
-    let words_filtered: any[] = []
-    let words = Tokenizer.word_tokenizer(sentence)
-    words.forEach((word: string) => {
-        let filter: any[]= '~`!@#$%^&*()-_=+[{]}\\|;:\'"/?.>,<'.split('')
-        if(!filter.includes(word)) {
-            words_filtered.push(word.toLowerCase())
-        }
-    })
-    console.log(words_filtered.length,'awal')
-    words_filtered  =words_filtered.filter(function(elem, pos) {
-        if(!results.includes(elem)) {
-            return words_filtered.indexOf(elem) == pos;
-        }
-    })
-    console.log(words_filtered.length,'akhir')
+let sentiments: {[i: string]: any} = {
+    negatif: [],
+    positif: []
+}
 
-    results = [...results,...words_filtered]
-})
 
-// 600235
-results.sort()
-console.log(results.slice(0,100))
-console.log(results.length,'done')
+// negatif.txt
+let negatif: any[] = fs.readFileSync('dataset/sentiment/negatif.txt').toString().split("\n")
+negatif = Array.from(new Set(negatif))
+negatif.sort()
 
-fs.appendFileSync('./normal.txt',results.join("\n"))
+
+// positif
+let positif: any[] = fs.readFileSync('dataset/sentiment/positif.txt').toString().split("\n")
+positif = Array.from(new Set(positif))
+positif.sort()
+
+// negatif = sentiments.negatif.map((data: any) => `${data.text}\t${data.point}`)
+// positif = sentiments.positif.map((data: any) => `${data.text}\t${data.point}`)
+
+
+fs.writeFileSync('dataset/sentiment/negatif.txt',negatif.join("\n"))
+fs.writeFileSync('dataset/sentiment/positif.txt',positif.join("\n"))
