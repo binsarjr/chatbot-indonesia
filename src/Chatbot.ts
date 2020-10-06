74bb50a2d036360e9d90493005cace2eb0986d9e
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2020-10-06 19:54:27 +0700

    Menghapus kode yang tidak digunakan

--- src/Chatbot.ts
+++ src/Chatbot.ts
@@ -17,20 +17,3 @@ class Chatbot {
         
     }
 }
-
-
-// Normalisasi data
-let negativePath = './dataset/sentiment/negatif.txt'
-let negative = fs.readFileSync(negativePath).toString().split('\n')
-negative = Array.from(new Set(negative))
-negative = negative.filter((el: string) => el != null && el != '')
-negative = negative.map((data: string) => `${data.split("\t")[0]}\t-1`)
-fs.writeFileSync(negativePath,negative.join("\n"))
-
-
-let positivePath = './dataset/sentiment/positif.txt'
-let positive = fs.readFileSync(positivePath).toString().split('\n')
-positive = Array.from(new Set(positive))
-positive = positive.filter((el: string) => el != null && el != '')
-positive = positive.map((data: string) => `${data.split("\t")[0]}\t1`)
-fs.writeFileSync(positivePath,positive.join("\n"))
